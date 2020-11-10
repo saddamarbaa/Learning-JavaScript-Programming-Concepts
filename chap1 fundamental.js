3098fda920e804031fe8cc8dc9a14fa90cef2936
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-11-10 21:59:18 +0700

    JavaScript Fundamentals

---
+++ chap1 fundamental.js
@@ -0,0 +1,56 @@
+// JavaScript Fundamentals
+// Statement
+alert("Hello, world!");
+
+/* An example with two messages.
+ This is a multiline comment.
+*/
+// alert("hello");
+// alert("World");
+alert("all fine now");
+[1, 2, 3].forEach(alert);
+// number
+console.log(4);
+console.log(100)
+console.log(221);
+typeof(12);
+
+// boolean (true/false)
+console.log(true);
+console.log(false)
+
+// string
+console.log("ok");
+console.log("Always Nice to see you");
+
+// math operators
+console.log((2 + 3) * 22);
+console.log("cs110 " + 22); // cs110 22 
+console.log(isNaN('cs110' * 12)); // True
+
+
+// compering values
+true === true;
+1 === 1;
+
+// Variables
+let message;
+message = 'Welcome dear'; // store the string
+console.log(message);
+
+let name = "Saddam"; // define variable and assign value
+let age = 30;
+message = "hello " + name; // value changed
+console.log(name);
+console.log(message)
+
+//Constants 
+const myBirthday = "1.1.1991";
+const PI = 3.14159;
+console.log(myBirthday);
+
+// JavaScript Logical Operators
+// Boolean Operators
+console.log(true && true); // AND
+console.log(false || true); // OR
+console.log(!(false && true)); // NOT
