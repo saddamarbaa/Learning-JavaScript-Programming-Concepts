323e37012b9257250747dc1dbaa4af2fd17756d7
saddamarbaa <saddamarbaas@gmail.com>
2020-11-18 21:25:05 +0700

    loop

---
+++ chap3 loops.js
@@ -0,0 +1,44 @@
+// JavaScript For Loop
+
+/*
+for loop syntax:
+for (statement 1; statement 2; statement 3) {
+  // code block to be executed
+}
+*/
+
+for (let i = 0; i <= 10; i++) {
+    // Runs 10 times, with values of step 0 through 9.
+    console.log(i);
+}
+
+// JavaScript While Loop statement
+
+/*
+Syntax
+while (condition) {
+  // code block to be executed
+}
+*/
+
+let j = 0;
+while (j < 10) {
+    console.log("the number is " + j);
+    j++;
+}
+
+// The Do/While Loop
+
+/*
+Syntax
+do {
+  // code block to be executed
+}
+while (condition);
+*/
+
+let k = 0;
+do {
+    console.log("the number is " + k);
+    k++;
+} while (k < 10);
