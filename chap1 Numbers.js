215d4488d6a257de77178da26c9773640eef692e
saddamarbaa <saddamarbaas@gmail.com>
2020-11-18 19:19:37 +0700

    Javscript Fundamental Numbers in Javascript

---
+++ chap1 Numbers.js
@@ -0,0 +1,22 @@
+// JavaScript Fundamentals
+
+// Numbers in Javascript
+
+var num1 = 10;
+
+// Increment num1 by 1
+num1++;
+
+// Decrement num1 by 1
+num1--;
+console.log(num1);
+
+// Divide, multiply *, remainder %
+console.log(num1 % 6);
+
+// Increment/decrement by 10
+num1 += 10;
+console.log(num1);
+
+num1 *= 2;
+console.log(num1);
