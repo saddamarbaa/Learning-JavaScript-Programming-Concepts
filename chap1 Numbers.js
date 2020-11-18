7610cc69f56a620d0f15deb1a0f944b35607cdbb
saddamarbaa <saddamarbaas@gmail.com>
2020-11-18 19:28:25 +0700

    Numbers in JavaScript

--- chap1 Numbers.js
+++ chap1 Numbers.js
@@ -2,7 +2,11 @@
 
 // Numbers in Javascript
 
-var num1 = 10;
+let num1 = 10;
+
+// floating numbers
+let num2 = 22.3;
+console.log(num2);
 
 // Increment num1 by 1
 num1++;
@@ -12,11 +16,13 @@ num1--;
 console.log(num1);
 
 // Divide, multiply *, remainder %
-console.log(num1 % 6);
+console.log(num1 % 6); // remainder %
 
-// Increment/decrement by 10
+// Increment/decrement  by any number you want  
+num1 = num2 + 10;
 num1 += 10;
 console.log(num1);
 
+//  multiply
 num1 *= 2;
 console.log(num1);
