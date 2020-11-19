d2fa35c1cf1445b86708a0ec7f89cee907cbf1b1
saddamarbaa <saddamarbaas@gmail.com>
2020-11-20 00:18:01 +0700

    indexO

--- chap3 Strings.js
+++ chap3 Strings.js
@@ -1,5 +1,12 @@
 // Strings (Common Methods)
 
-let furit = "banana"
-let moreFruit = "banana\napple";
+let fruit = "banana"
+let moreFruit = "banana\napple"; // new line
 console.log(moreFruit);
+
+// furit.length
+console.log(fruit.length); // get length
+
+console.log(fruit.indexOf('b')); // return 0
+console.log(fruit.indexOf('m')); // return -1
+console.log(fruit.indexOf('n')); // return 2
