5a1918b2cc62de766d3696080083bae0cdc29cbf
saddamarbaa <saddamarbaas@gmail.com>
2020-11-19 20:59:02 +0700

    The String Data Type

--- chap1 Data Types.js
+++ chap1 Data Types.js
@@ -1,1 +1,49 @@
+// Data Types in JavaScript
 
+// The Number Data Type
+// integer(A number without decimals)
+var num = 25;
+console.log(num);
+
+// floating-point number(A number with decimals)
+const PI = 3.14;
+var num1 = 80.5;
+console.log(PI);
+console.log(num1);
+
+// exponential notation, same as 4.25e6 or 4250000
+var num2 = 4.25e+6;
+console.log(num2);
+
+// exponential notation, same as 0.00000425
+var num3 = 4.25e-6;
+console.log(num3);
+
+var x = 0.2 + 0.1;
+console.log(x);
+
+// The Boolean Data Type
+
+let isCodingFun = true; // yes coding is fun
+let isFishTasty = false; // no is not tasty
+let isJavaScriptFun = true; // yes js is fun
+console.log(isCodingFun); // Outputs  (true)
+console.log(isJavaScriptFun); // Outputs  (true)
+console.log(isFishTasty); // Outputs (false)
+
+// Boolean Expression
+let number1 = 10;
+let number2 = 9;
+// returns true, because 10 is higher than 9
+console.log(number1 > number2);
+
+// returns false, because 10 is not equal to 9
+console.log(number1 === number2);
+
+// The String Data Type
+// Create a string variable
+let greet = 'Hello'; // using single quotes
+let greeting = "Hello World"; // using double quotes
+let firstName = "Sa'dd'am"; // single quote inside double quotes
+let secondName = 'Sadd"a"m'; // double quotes inside single quotes
+let love = 'I\'love javscript.'; // escaping single quote with backslash
