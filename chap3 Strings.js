0c22d96ea5182c08a1ad87bcd4e323d13d144b9c
saddamarbaa <saddamarbaas@gmail.com>
2020-11-20 00:57:38 +0700

    JavaScript Strings

--- chap3 Strings.js
+++ chap3 Strings.js
@@ -1,11 +1,26 @@
+// JavaScript Strings
 // Strings (Common Methods)
+
+// Create a string variable
+let greet = 'Hello'; // using single quotes
+let address = '123 Batam Indonesia'; // using single quotes
+let greeting = "Hello World"; // using double quotes
+let firstName = "Sa'dd'am"; // single quote inside double quotes
+let secondName = 'Sadd"a"m'; // double quotes inside single quotes
+let love = 'I\'love javscript.'; // escaping single quote with backslash
+console.log(love);
 
 let fruit = "banana"
 let moreFruit = "banana\napple"; // new line
 console.log(moreFruit);
 
-// furit.length
-console.log(fruit.length); // get length
+// String Length
+
+let txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let sln = txt.length; // get length
+console.log(sln);
+console.log(txt.length); // return 26
+console.log(fruit.length); // return 26
 
 console.log(fruit.indexOf('b')); // return 0
 console.log(fruit.indexOf('m')); // return -1
