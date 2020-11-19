11999517315f08d39766bce559b8acbdec59ed9a
saddamarbaa <saddamarbaas@gmail.com>
2020-11-20 00:21:07 +0700

    slice

--- chap3 Strings.js
+++ chap3 Strings.js
@@ -10,3 +10,6 @@ console.log(fruit.length); // get length
 console.log(fruit.indexOf('b')); // return 0
 console.log(fruit.indexOf('m')); // return -1
 console.log(fruit.indexOf('n')); // return 2
+
+console.log(fruit.slice(2, 6)); // return nana
+console.log(fruit.slice(0, 3)); // return ban
