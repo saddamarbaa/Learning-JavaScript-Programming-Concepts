12bcd02ad2d44385b4aa42efe76177a47dd94552
saddamarbaa <saddamarbaas@gmail.com>
2020-11-20 12:39:02 +0700

    Accessing the Elements of an Array

--- chap3 Arrays.js
+++ chap3 Arrays.js
@@ -8,3 +8,8 @@ let colors = ["Red", "Green", "Blue"];
 
 // Accessing the Elements of an Array
 console.log(marks); // print: [ 20, 30, 40, 50, 60 ]
+console.log(colors); // print: [ 'Red', 'Green', 'Blue' ]
+console.log(fruits); // print:[ 'Apple', 'Banana', 'Mango', 'Orange', 'Papaya' ]
+console.log(colors[0]); // print: Red
+console.log(colors[1]); // print: Green
+console.log(colors[2]); // print: Blue
