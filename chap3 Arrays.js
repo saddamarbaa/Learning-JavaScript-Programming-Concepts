28f331d564556d84e5ef629ec869b391b0a70739
saddamarbaa <saddamarbaas@gmail.com>
2020-11-20 12:35:49 +0700

    Creating an Array

--- chap3 Arrays.js
+++ chap3 Arrays.js
@@ -1,1 +1,10 @@
-// https://www.tutorialrepublic.com/javascript-tutorial/javascript-arrays.php
+// JavaScript Arrays
+
+// Creating an Array
+// Array: Declaration with Initialization
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Papaya"];
+let marks = [20, 30, 40, 50, 60];
+let colors = ["Red", "Green", "Blue"];
+
+// Accessing the Elements of an Array
+console.log(marks); // print: [ 20, 30, 40, 50, 60 ]
