70eb449585bb50e688e4f139e58579d34204236c
saddamarbaa <saddamarbaas@gmail.com>
2020-11-20 11:42:08 +0700

    JavaScript Objects

--- chap3 Objects.js
+++ chap3 Objects.js
@@ -13,11 +13,22 @@ const data = { // an object
         city: "Khartoum",
         coutry: "Sudan",
     },
-    lov: "Music"
+    love: "Music"
 };
 
+// Using the JavaScript Keyword new
 
-var person = {
+
+console.log(data.name); // print : Saddam Arbaa
+console.log(data.age); // print :30
+console.log(data.love); // print : Music
+// print : { name: 'Khartoum University', city: 'Khartoum', coutry: 'Sudan' }
+console.log(data.organization);
+console.log(data.organization.name); // print :Khartoum University
+console.log(data.organization.coutry); // print : Sudan
+console.log(data.organization.city); // print : Khartoum
+
+let person = {
     name: "Ali",
     age: 32,
     gender: "Male",
@@ -26,11 +37,17 @@ var person = {
     }
 };
 
-console.log(data.name); // print : Saddam Arbaa
-console.log(data.age); // print :30
-console.log(data.lov); // print : Music
-// print : { name: 'Khartoum University', city: 'Khartoum', coutry: 'Sudan' }
-console.log(data.organization);
-console.log(data.organization.name); // print :Khartoum University
-console.log(data.organization.coutry); // print : Sudan
-console.log(data.organization.city); // print : Khartoum
+// Accessing Object's Properties
+
+let book = {
+    "name": "Harry Potter and the Goblet of Fire",
+    "author": "J. K. Rowling",
+    "year": 2000
+};
+
+// Dot notation
+console.log(book.author); // Prints: J. K. Rowling
+
+// Bracket notation
+console.log(book["year"]); // Prints: 2000
+console.log(book['name']); // Prints: Harry Potter and the Goblet of Fire
