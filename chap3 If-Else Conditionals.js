514c8a348d2c3025d4567b69dea4807839350f4a
saddamarbaa <saddamarbaas@gmail.com>
2020-11-21 12:58:26 +0700

    The if Statement

--- chap3 If-Else Conditionals.js
+++ chap3 If-Else Conditionals.js
@@ -1,4 +1,4 @@
-// JavaScript if else and else if(Control Flows)
+// JavaScript Conditional Statements (Control Flows)
 
 // The if Statement
 
@@ -7,5 +7,10 @@ Syntax
 if (condition) {
   //  block of code to be executed if the condition is true
 }
-
 */
+
+let now = new Date();
+let dayOfWeek = now.getDay(); // Sunday - Saturday : 0 - 6
+if (dayOfWeek === 5) {
+    console.log("Have a nice weekend!");
+}
