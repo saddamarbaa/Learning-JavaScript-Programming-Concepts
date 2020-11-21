713c8223342b02e6d40212a224b479c950fcd742
saddamarbaa <saddamarbaas@gmail.com>
2020-11-21 13:01:00 +0700

    The if Statement

--- chap3 If-Else Conditionals.js
+++ chap3 If-Else Conditionals.js
@@ -14,3 +14,8 @@ let dayOfWeek = now.getDay(); // Sunday - Saturday : 0 - 6
 if (dayOfWeek === 5) {
     console.log("Have a nice weekend!");
 }
+
+const name = "Sadam";
+if (name === "Sadam") {
+    console.log("hi " + name);
+}
