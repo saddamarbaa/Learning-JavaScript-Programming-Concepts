fb32d99ae32f149b8d443434377298e00cf8a584
saddamarbaa <saddamarbaas@gmail.com>
2020-11-21 17:29:08 +0700

    switch statement to check size

--- chap3 Switch Statements.js
+++ chap3 Switch Statements.js
@@ -22,25 +22,25 @@ let dayOfWeek = now.getDay(); // Sunday - Saturday : 0 - 6
 // console.log(dayOfWeek); // for testing
 switch (dayOfWeek) {
     case 0:
-        console.log("Monday");
+        console.log("Today is Monday");
         break;
     case 1:
-        console.log("Tuesday");
+        console.log("Today is Tuesday");
         break;
     case 2:
-        console.log("Wednesday");
+        console.log("Today is Wednesday");
         break;
     case 3:
-        console.log("Thursday");
+        console.log("Today is Thursday");
         break;
     case 4:
-        console.log("Friday")
+        console.log("Today is Friday")
         break;
     case 5:
-        console.log("Saturday\nhappy weekend");
+        console.log("Today is Saturday\nhappy weekend");
         break;
     case 6:
-        console.log("Sunday\n happy weekend");
+        console.log("Today is Sunday\n happy weekend");
         break;
 
     default:
@@ -48,3 +48,27 @@ switch (dayOfWeek) {
         break;
         // not need break statement after default case I added only for readability
 }
+
+// switch statement to check size
+let Number = 40;
+let size;
+switch (Number) {
+    case 30:
+        size = "Small";
+        break;
+    case 40:
+        // match the value of this case
+        size = "Medium";
+        break;
+    case 42:
+        size = "Large";
+        break;
+    case 47:
+        size = "Extra Large";
+        break;
+    default:
+        size = "its Unknown size";
+        break;
+        // not need break statement after default case I added only for readability
+}
+console.log(size);
