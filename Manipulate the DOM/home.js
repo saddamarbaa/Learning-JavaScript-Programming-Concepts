cbb94c16c33691c72aa4d88fcdb5b647435586fc
saddamarbaa <saddamarbaas@gmail.com>
2020-11-25 23:38:16 +0700

    addEventListener

--- Manipulate the DOM/home.js
+++ Manipulate the DOM/home.js
@@ -6,3 +6,30 @@ console.log("hello world");
 console.log("hello there");
 // alert("Yeoo");
 console.log("hello this Saddam");
+
+
+let container = document.getElementById("container");
+// container.innerHTML = "new text";
+
+// return 3
+let len = container.children.length;
+console.log(len);
+
+
+// access container.children[1]  
+container.children[0].innerHTML = "firsr child";
+
+// access container.children[1] 
+container.children[1].innerHTML = "second child";
+
+const puppy = container.children[1];
+
+//  addEventListener
+document.addEventListener('click', function() {
+    container.children[0].innerHTML = '123';
+});
+
+
+document.addEventListener("click", function() {
+    container.children[1].innerHTML = "Hello World";
+});
