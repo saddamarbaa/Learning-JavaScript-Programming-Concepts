e7b60f936ca0724cf696ab7bf351e268d66cb712
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2021-01-04 18:56:41 +0700

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-// 5. Sort the inventors by years lived
-// const yearslived = inventors.sort(function (a,b) {
-// 	if (a.passed-a.year>b.passed-b.year) {
-// 		return 1
-// 	}else{
-// 		return -1;
-// 	}
-// })
