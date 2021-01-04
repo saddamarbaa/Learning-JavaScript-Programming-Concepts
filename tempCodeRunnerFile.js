ea9c06057295c442631ea45b4ee9a2df570a8db4
saddamarbaa <saddamarbaas@gmail.com>
2021-01-04 18:54:35 +0700

    04 - Array Cardio Day 1(challenge 4 of 30)

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,8 @@
-forEach
+// 5. Sort the inventors by years lived
+// const yearslived = inventors.sort(function (a,b) {
+// 	if (a.passed-a.year>b.passed-b.year) {
+// 		return 1
+// 	}else{
+// 		return -1;
+// 	}
+// })
